b663f2074b3f703a6f69eba5e02079c0e45ffdbb
VanessaSOliveira <vanessasouzaoliveira1@gmail.com>
2018-08-08 20:57:07 -0300

    ultimas alterações

---
+++ Projeto3_IA/src/projeto3_ia/Fuzzy.java
@@ -0,0 +1,73 @@
+
+package projeto3_ia;
+
+import net.sourceforge.jFuzzyLogic.FIS;
+import net.sourceforge.jFuzzyLogic.FunctionBlock;
+
+/**
+ *
+ * @author Vanessa
+ */
+public class Fuzzy {
+    //private Double dx, dy; // entradas
+    private Double velocidade, angulo;// saidas
+    
+    
+    public Fuzzy(){
+        
+    }
+    
+    
+    //chama esse metodo e depois pega as saidas pelo get
+    public void calculaVelocidadeEAngulo(Double dx, Double dy){
+        String filename = "arquivo.fcl";
+        FIS fis = FIS.load(filename, true);
+
+        if (fis == null) {
+            System.err.println("Can't load file: '" + filename + "'");
+            System.exit(1);
+	}
+
+	// Get default function block
+	FunctionBlock fb = fis.getFunctionBlock(null);
+              ////////////n sei se ate essa parte tem q repetir toda vez 
+
+	// Set inputs
+	fb.setVariable("dx", dx);
+	fb.setVariable("dy", dy);
+                
+
+	// Evaluate
+	fb.evaluate();
+
+	// Show output variable's chart
+	fb.getVariable("Velocidade").defuzzify();
+        fb.getVariable("Angulo").defuzzify();
+
+	// Print ruleSet
+	//System.out.println(fb);
+        
+        this.velocidade = fb.getVariable("Velocidade").getValue();
+        this.angulo = fb.getVariable("Angulo").getValue();
+        
+	/*System.out.println("Velocidade: " + this.velocidade +
+            " Angulo: " + this.angulo);*/
+    }
+    
+    public Double getVelocidade(){
+        return this.velocidade;
+    }
+    
+    public Double getAngulo(){
+        return this.angulo;
+    }
+    
+    public static void main(String[] args){
+        Fuzzy fuzzy = new Fuzzy();
+        fuzzy.calculaVelocidadeEAngulo(100.0, 30.0);
+        System.out.println("Velocidade igual a : " + fuzzy.getVelocidade());
+        System.out.println("Angulo igual a : " + fuzzy.getAngulo());
+        
+    }
+    
+}
--- Projeto3_IA/src/projeto3_ia/Sistema.java
+++ Projeto3_IA/src/projeto3_ia/Sistema.java
@@ -76,3 +76,92 @@ public class Sistema {
     }
 
 }
+
+/*
+
+public class Sistema {
+    
+    private int Xr, Yr, Xa, Ya, Yc;
+    private double angulo, velocidade;
+    
+    public Sistema(double angulo, double velocidade, int Yc){
+        this.angulo = angulo;
+        this.velocidade = velocidade;
+        this.Yc = Yc;
+        this.Xa=5;
+        this.Ya=3;
+    }
+    
+    public void entradas(double angulo, double velocidade){
+        this.angulo = angulo;
+        this.velocidade = velocidade;
+    }
+    
+    private double calculaVelocidadeX(double velocidadeInicial, double angulo){
+        //Vx = V0 cos(Ao)
+        double rad = (Math.PI/180) * angulo;
+        double cos = Math.cos(rad);
+        
+        return velocidadeInicial * cos;
+    }
+    
+    private double calculaVelocidadeY(double velocidadeInicial, double angulo){
+        //Vy = V0 sen(Ao)
+        double rad = (Math.PI/180) * angulo;
+        double seno = Math.sin(rad);
+        
+        return velocidadeInicial * seno;
+    }
+    
+    private double calculaX(double tempo, double velocidadeX){ //o principal
+        //x(t) = Vx t
+        System.out.println("Calcula x resultado : " + velocidadeX*tempo);
+        return velocidadeX * tempo;
+    }
+    
+    private double calculaY(double tempo, double velocidadeY){ //o principal
+        //y(t) = Yc + Vy t - gt^2/2
+
+        double y =  Yc + (velocidadeY * tempo) - (9.8 * (Math.pow(tempo, 2)/2));
+        System.out.println("resultado y " + y);
+        return y;
+    }
+    
+    private double calculaTempoY(double vx){
+        //
+        System.out.println("resultado calculatempoy " + Xa/vx);
+        return Xa / vx;
+    }
+    
+    private double calculaTempoX(double velocidadeY){
+        System.out.println("resultado calculatempox " + Math.sqrt(2*(Yc - velocidadeY) / 9.8));
+        return Math.sqrt(2*(Yc - velocidadeY) / 9.8);
+    }
+    
+    private double calculaXr(){
+        return calculaX(calculaTempoX(calculaVelocidadeY(velocidade,angulo)),calculaVelocidadeX(velocidade,angulo));
+    }
+    
+    private double calculaYr(){
+        return calculaY(calculaTempoY(calculaVelocidadeX(velocidade,angulo)), calculaVelocidadeY(velocidade,angulo));
+    }
+
+    public double diferencaDx(){ //principal
+        return calculaXr() - Xa;
+    }
+    
+    public double diferencaDy(){ //principal
+        return calculaYr() - Ya;
+    }
+
+    public static void main(String[] args){
+        int velocidade=10, angulo=20, yc=1;
+        Sistema s = new Sistema(angulo,velocidade,yc);
+        Double dx = s.diferencaDx();
+        Double dy = s.diferencaDy();
+        System.out.println("Dx: " + dx + "Dy : " + dy);
+    }
+}
+
+
+*/
